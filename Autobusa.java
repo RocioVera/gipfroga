c7f36677fddc6ae791c1f7e4c9ea16254edf9db6
RocioVera <46562633+RocioVera@users.noreply.github.com>
2019-01-10 13:33:24 +0100

    Add files via upload

---
+++ Autobusa.java
@@ -0,0 +1,14 @@
+
+public class Autobusa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=1;
+		int y=5;
+		int gehiketa;
+		gehiketa=x+y;
+		System.out.println(gehiketa);
+
+	}
+
+}
